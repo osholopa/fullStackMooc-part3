cb9736be9fa74417fd9c916bf9037c1debe214b3
Oskari Holopainen <oskariholopa@gmail.com>
2020-01-03 23:33:08 +0200

    completed excercises until 3.10

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
+const cors = require("cors")
 
 app.use(bodyParser.json())
+app.use(cors())
 
 morgan.token("request-body", function(req, res) {
   if(req.body.name) {
